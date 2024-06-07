c4e2f7b6d1f002104e5cd6c1c946e52eedec9d07
Vadym <vadymlysytskyi@gmail.com>
2024-06-07 22:36:52 +0200

    fix getContactById

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -6,6 +6,6 @@ export const getAllContacts = async () => {
 };
 
 export const getContactById = async (contactId) => {
-  const contact = await ContactsCollection.find({ _id: contactId });
-  return contact[0];
+  const contact = await ContactsCollection.findById(contactId);
+  return contact;
 };
