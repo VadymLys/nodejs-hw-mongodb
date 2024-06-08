535bdac2283c19d508bc2c1830a8d257ea14acfa
Vadym <vadymlysytskyi@gmail.com>
2024-06-08 22:43:29 +0200

    fix deletecontroller

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -14,7 +14,7 @@ export const getAllContactsController = async (req, res) => {
 
     res.json({
       status: 200,
-      message: 'Successfully found students!',
+      message: 'Successfully found contacts!',
       data: contacts,
     });
   } catch (err) {
@@ -33,18 +33,18 @@ export const getContactByIdController = async (req, res, next) => {
     if (!mongoose.Types.ObjectId.isValid(contactId)) {
       return res.status(404).json({
         status: 404,
-        message: 'Not found',
+        message: 'Contact not found',
       });
     }
     const contact = await getContactById(contactId);
 
     if (!contact) {
-      next(createHttpError(404, 'Student not found'));
+      next(createHttpError(404, 'Contact not found'));
     }
 
     res.json({
       status: 200,
-      message: `Successfully found student with id ${contactId}!`,
+      message: `Successfully found contact with id ${contactId}!`,
       data: contact,
     });
   } catch (err) {
@@ -61,7 +61,7 @@ export const createContactController = async (req, res) => {
 
   res.status(201).json({
     status: 201,
-    message: `Successfully created a student!`,
+    message: `Successfully created a contact!`,
     data: contact,
   });
 };
@@ -71,11 +71,11 @@ export const deleteContactController = async (req, res, next) => {
   const contact = await deleteContact(contactId);
 
   if (!contact) {
-    next(createHttpError(404, 'Student not found'));
+    next(createHttpError(404, 'Contact not found'));
     return;
   }
 
-  res.status(204).send();
+  res.status(204).json({ message: 'Delete success' }).send();
 };
 
 export const upsertContactController = async (req, res, next) => {
@@ -85,14 +85,14 @@ export const upsertContactController = async (req, res, next) => {
     upsert: true,
   });
   if (!result) {
-    next(createHttpError(404), 'Student not found');
+    next(createHttpError(404), 'Contact not found');
     return;
   }
   const status = result.isNew ? 201 : 200;
 
   res.status(status).json({
     status,
-    message: `Successfully upserted a student!`,
+    message: `Successfully upserted a contact!`,
     data: result.contact,
   });
 };
@@ -102,12 +102,12 @@ export const patchContactController = async (req, res, next) => {
   const result = await updateContact(contactId, req.body);
 
   if (!result) {
-    next(createHttpError(404, 'Student not found'));
+    next(createHttpError(404, 'Contact not found'));
   }
 
   res.json({
     status: 200,
-    message: `Successfully patched a student!`,
+    message: `Successfully patched a contact!`,
     data: result.student,
   });
 };
