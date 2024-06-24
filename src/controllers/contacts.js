b68f41d3f08c474273ed9ab5a840d2ae66166337
Vadym <vadymlysytskyi@gmail.com>
2024-06-25 00:19:00 +0200

    add cloudinary

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -11,6 +11,8 @@ import { parsePaginationParams } from '../utils/parsePaginationParams.js';
 import { parseSortParams } from '../utils/parseSortParams.js';
 import { parseFilterParams } from '../utils/parseFilterParams.js';
 import { saveFileToUploadDir } from '../utils/saveFileToUploadDir.js';
+import { saveFileToCloudinary } from '../utils/saveFileToCloudinary.js';
+import { env } from '../utils/env.js';
 
 export const getAllContactsController = async (req, res) => {
   try {
@@ -77,8 +79,18 @@ export const getContactByIdController = async (req, res, next) => {
 
 export const createContactController = async (req, res) => {
   const userId = req.user._id;
+  const photo = req.file;
+  let photoUrl;
 
-  const contact = await createContact({ ...req.body, userId });
+  if (photo) {
+    if (env('ENABLE_CLOUDINARY') === 'true') {
+      photoUrl = await saveFileToCloudinary(photo);
+    } else {
+      photoUrl = await saveFileToUploadDir(photo);
+    }
+  }
+
+  const contact = await createContact({ ...req.body, userId, photo: photoUrl });
 
   res.status(201).json({
     status: 201,
@@ -104,14 +116,9 @@ export const upsertContactController = async (req, res, next) => {
   const { contactId } = req.params;
   const userId = req.user._id;
 
-  const result = await updateContact(
-    contactId,
-    req.body,
-    {
-      upsert: true,
-    },
-    userId,
-  );
+  const result = await updateContact(contactId, userId, req.body, {
+    upsert: true,
+  });
 
   if (!mongoose.Types.ObjectId.isValid(contactId)) {
     return res.status(404).json({
@@ -140,7 +147,11 @@ export const patchContactController = async (req, res, next) => {
   let photoUrl;
 
   if (photo) {
-    photoUrl = await saveFileToUploadDir(photo);
+    if (env('ENABLE_CLOUDINARY') === 'true') {
+      photoUrl = await saveFileToCloudinary;
+    } else {
+      photoUrl = await saveFileToUploadDir(photo);
+    }
   }
 
   const result = await updateContact(contactId, userId, {
@@ -159,6 +170,7 @@ export const patchContactController = async (req, res, next) => {
 
   if (!result) {
     next(createHttpError(404, 'Contact not found'));
+    return;
   }
 
   res.json({
