6df1da2d3349b4f3441665373028f6bc659ec160
Vadym <vadymlysytskyi@gmail.com>
2024-06-29 11:31:37 +0200

    first commit google oauth

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -2,12 +2,15 @@ import { Router } from 'express';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import {
   loginUserSchema,
+  loginWithGoogleOAuthSchema,
   registerUserSchema,
   requestResetEmailSchema,
   resetPasswordSchema,
 } from '../validation/auth.js';
 import {
+  getGoogleOAuthUrlController,
   loginUserController,
+  loginWithGoogleController,
   logoutUserController,
   refreshUserSessionController,
   registerUserController,
@@ -46,4 +49,11 @@ router.post(
   ctrlWrapper(resetPasswordController),
 );
 
+router.get('/get-oauth-url', ctrlWrapper(getGoogleOAuthUrlController));
+
+router.post(
+  '/confirm-oauth',
+  validateBody(loginWithGoogleOAuthSchema),
+  ctrlWrapper(loginWithGoogleController),
+);
 export default router;
---
+++ src/utils/googleOAuth2.js
@@ -0,0 +1,49 @@
+import { OAuth2Client } from 'google-auth-library';
+import path from 'node:path';
+import { readFile } from 'fs/promises';
+
+import { env } from '../utils/env.js';
+import createHttpError from 'http-errors';
+
+const PATH_JSON = path.join(process.cwd(), 'google-oauth.json');
+
+const oauthConfig = JSON.parse(await readFile(PATH_JSON));
+console.log('🚀 ~ oauthConfig:', oauthConfig);
+
+const googleOAuthClient = new OAuth2Client({
+  clientId: env('GOOGLE_AUTH_CLIENT_ID'),
+  clientSecret: env('GOOGLE_AUTH_CLIENT_SECRET'),
+  redirectUri: oauthConfig.web.redirect_uris[0],
+});
+console.log('🚀 ~ googleOAuthClient:', googleOAuthClient);
+
+export const generateAuthUrl = () => {
+  const authUrl = googleOAuthClient.generateAuthUrl({
+    scope: [
+      '<https://www.googleapis.com/auth/userinfo.email>',
+      '<https://www.googleapis.com/auth/userinfo.profile>',
+    ],
+  });
+  console.log('🚀 ~ generateAuthUrl ~ authUrl:', authUrl);
+  return authUrl;
+};
+
+export const validateCode = async (code) => {
+  const response = await googleOAuthClient.getToken(code);
+  if (!response.tokens.id_token) throw createHttpError(401, 'Unauthorized');
+
+  const ticket = await googleOAuthClient.verifyIdToken({
+    idToken: response.tokens.id_token,
+  });
+  return ticket;
+};
+
+export const getFullNameFromGoogleTokenPayload = (payload) => {
+  let fullName = 'Guest';
+  if (payload.given_name && payload.family_name) {
+    fullName = `${payload.given_name} ${payload.family_name}`;
+  } else if (payload.given_name) {
+    fullName = payload.given_name;
+  }
+  return fullName;
+};
